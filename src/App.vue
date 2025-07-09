0f380a5f253a0ce7d2a36ce2ba8ed462a179d1a5
jon <lil.elma132@gmail.com>
2025-07-09 22:16:11 +0800

    progress in crashcourse

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,26 @@
-<script>
-export default {
-  setup (){
-    const name = 'John Doe';
-    const status = 'active';
-    const tasks = ['Task 1', 'Task 2', 'Task 3'];
-
-    const toggleStatus = () => {
-      if (this.status === 'active') {
-        this.status = 'pending';
-      }else if(this.status === 'pending'){
-        this.status = 'inactive';
-      }else {
-        this.status = 'active';
+<script setup>
+import {ref} from 'vue';
+const name = ref('John Doe');
+const status = ref('active');
+const tasks = ref(['Task 1', 'Task 2', 'Task 3']);
+const newTask = ref('test');
+const toggleStatus = () => {
+  if (status.value === 'active') {
+      status.value = 'pending';
+    }else if(status.value === 'pending'){
+      status.value = 'inactive';
+    }else {
+      status.value = 'active';
       }
-    };
-
-    return {
-      name,
-      status,
-      tasks,
-      toggleStatus,
-    }
-  },
+};
+const addTask = () => {
+  if (newTask.value.trim() != ''){
+    tasks.value.push(newTask.value);
+    newTask.value = '';
+  }
+};
+const deleteTask = (index) => {
+  tasks.value.splice(index,1);
 };
 </script>
 
@@ -30,10 +29,17 @@ export default {
   <p v-if="status == 'active'">User is Active</p>
   <p v-else-if="status == 'pending'">User is pending</p>
   <p v-else>User is inactive</p>
-
+  <form @submit.prevent="addTask">
+    <label for="newTask"></label>
+    <input type="text" id="newTask" name="newTask" v-model="newTask">
+    <button type="submit"></button>
+  </form>
   <h3>Task:</h3>
   <ul>
-    <li v-for="task in tasks" :key="task">{{ task }}</li>
+    <li v-for="(task, index) in tasks" :key="task">
+      <span>{{ task }}</span>
+      <button @click="deleteTask(index)">x</button>
+    </li>
   </ul>
   <!-- <button v-on:click="toggleStatus">Change Status</button> -->
   <button v-on:click="toggleStatus">Change Status</button>
